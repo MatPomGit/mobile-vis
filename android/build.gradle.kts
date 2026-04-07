1d48b38c5247321cb2b03b1475ae0059cedb564e
Mateusz Pomianek <matpomianek@gmail.com>
2026-04-07 04:01:17 +0200

    15

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,5 +2,5 @@
 plugins {
     alias(libs.plugins.android.application) apply false
 }
-val app_version_name by extra("1.14")
-val app_version_code by extra(14)
+val app_version_name by extra("1.15")
+val app_version_code by extra(15)
